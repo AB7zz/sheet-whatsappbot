439cfc9753078b7dce5f4c6117622b9cb9b73060
AB7zz <abhinavcv007@gmail.com>
2023-11-12 13:01:07 +0530

    function verifyWebhook

--- routes/whatsapp.js
+++ routes/whatsapp.js
@@ -8,23 +8,6 @@ dotenv.config()
 const router = express.Router()
 
 router.post('/webhook', insertToSheet)
-router.get('/webhook', (req, res) => {
-    try{
-      const verify_token = process.env.VERIFY_TOKEN;
-      let mode = req.query["hub.mode"];
-      let token = req.query["hub.verify_token"];
-      let challenge = req.query["hub.challenge"];
-      if (mode && token) {
-        if (mode === "subscribe" && token === verify_token) {
-          console.log("WEBHOOK_VERIFIED");
-          res.status(200).send(challenge);
-        } else {
-          res.sendStatus(403);
-        }
-      }
-    } catch (err) {
-      throw err;
-    }
-})
+router.get('/webhook', verifyWebhook)
 
 export default router
