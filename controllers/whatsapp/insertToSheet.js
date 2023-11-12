38a86f8505d0eb0c11e5b15c34f9f5d3e2336024
AB7zz <abhinavcv007@gmail.com>
2023-11-12 13:05:59 +0530

    removed 2 res

--- controllers/whatsapp/insertToSheet.js
+++ controllers/whatsapp/insertToSheet.js
@@ -1,51 +1,49 @@
 import dotenv from 'dotenv'
-dotenv.config()
 import doc from '../../config/index.js';
 import axios from 'axios'
 
+dotenv.config()
+
 async function insertToSheet(req, res) {
   try{
-    let body = req.body;
     const token = process.env.WHATSAPP_TOKEN
-    console.log(JSON.stringify(req.body, null, 2));
     if (req.body.object) {
-    if (
-      req.body.entry &&
-      req.body.entry[0].changes &&
-      req.body.entry[0].changes[0] &&
-      req.body.entry[0].changes[0].value.messages &&
-      req.body.entry[0].changes[0].value.messages[0]
-    ) {
-      let phone_number_id =
-        req.body.entry[0].changes[0].value.metadata.phone_number_id;
-      console.log(phone_number_id);
-      let from = req.body.entry[0].changes[0].value.messages[0].from;
-      let msg = req.body.entry[0].changes[0].value.messages[0].text.body;
-      axios({
-        method: "POST",
-        url:
-          "https://graph.facebook.com/v12.0/" +
-          phone_number_id +
-          "/messages?access_token=" +
-          token,
-        data: {
-          messaging_product: "whatsapp",
-          to: from,
-          text: { body: "Inserted " + msg + " into google sheets successfully!" },
-        },
-        headers: { "Content-Type": "application/json" },
-      });
-      const sheet = doc.sheetsByTitle['Sheet1'];
-      await sheet.addRow({ msg });
-      res.send('Successfully added to sheet')
+      if (
+        req.body.entry &&
+        req.body.entry[0].changes &&
+        req.body.entry[0].changes[0] &&
+        req.body.entry[0].changes[0].value.messages &&
+        req.body.entry[0].changes[0].value.messages[0]
+      ) 
+      {
+        let phone_number_id =
+          req.body.entry[0].changes[0].value.metadata.phone_number_id;
+        let from = req.body.entry[0].changes[0].value.messages[0].from;
+        let msg = req.body.entry[0].changes[0].value.messages[0].text.body;
+        axios({
+          method: "POST",
+          url:
+            "https://graph.facebook.com/v12.0/" +
+            phone_number_id +
+            "/messages?access_token=" +
+            token,
+          data: {
+            messaging_product: "whatsapp",
+            to: from,
+            text: { body: "Inserted " + msg + " into google sheets successfully!" },
+          },
+          headers: { "Content-Type": "application/json" },
+        });
+        const sheet = doc.sheetsByTitle['Sheet1'];
+        await sheet.addRow({ msg });
+        res.send('Successfully added to sheet')
+      }
+    } else {
+      res.sendStatus(404);
     }
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
+  } catch (err) {
+    throw err;
   }
-    } catch (err) {
-      throw err;
-    }
 }
 
 export default insertToSheet
