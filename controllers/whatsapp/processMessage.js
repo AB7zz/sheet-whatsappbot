81e9044c2d0a528816ee983afab3a8963b046b3a
AB7zz <abhinavcv007@gmail.com>
2024-01-13 00:17:55 +0530

    list title input

--- controllers/whatsapp/processMessage.js
+++ controllers/whatsapp/processMessage.js
@@ -193,7 +193,7 @@ async function processMessage(req, res) {
         let phone_number_id =
           req.body.entry[0].changes[0].value.metadata.phone_number_id;
         let from = req.body.entry[0].changes[0].value.messages[0].from;
-        msg = req.body.entry[0].changes[0].value.messages[0]?.text?.body || req.body.entry[0].changes[0].value.messages[0]?.interactive?.button_reply.title
+        msg = req.body.entry[0].changes[0].value.messages[0]?.text?.body || req.body.entry[0].changes[0].value.messages[0]?.interactive?.button_reply.title || req.body.entry[0].changes[0].value.messages[0]?.interactive?.list_reply.title
         
         // If the message is neither a message nor a button reply, then it is an image. So I'm doing the following
         /*
